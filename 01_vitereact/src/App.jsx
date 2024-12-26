d95209727c510bd37a7e896be8aae9bc8c34db2e
Punit <punitkumar4328@gmail.com>
2024-12-26 17:00:05 +0530

    custom react

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,19 +1,15 @@
-import Chai from "./chai"
-
-
-
+import Chai from "./chai";
 
 function App() {
- 
+  const username = "punit";
 
   return (
     <>
-      <Chai/>  
-      <h1>hello world</h1>
+      <Chai />
+      <h1>hello {username}</h1> //evaluated expression
       <p>test para</p>
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ customReact/customreact.js
@@ -0,0 +1,33 @@
+function customRender(reactElement, countainer){
+
+
+    // const domElement = document.createElement
+    // (reactElement.type)
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
+
+    // countainer.appendChild(domElement)
+
+   const domElement =  document.createElement(reactElement.type)
+   domElement.innerHTML = reactElement.children
+   for (const prop in reactElement.props) {
+   if(prop == 'children') continue;
+   domElement.setAttribute(prop, reactElement.props[prop])
+   }
+   countainer.appendChild(domElement)
+    
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const mainCountainer = document.querySelector('#root')
+
+customRender(reactElement, mainCountainer)
